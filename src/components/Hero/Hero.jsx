a5b67a7bf5a2a9aa3da164aa1f532c744b75167e
leonardojpires <leonardojpires2006@gmail.com>
2026-02-10 21:25:58 +0000

    added responsivity to the hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ import personalImage from "../../../public/imgs/hero/personal_picture.png";
 export default function Hero() {
     return (
         <section className="pt-30 bg-linear-to-t from-(--gray-50) to-(--color-secondary)/10 flex justify-center min-h-screen">
-            <div className="hero-section">
+            <div className="hero-section px-5">
                 <div className="w-full md:w-1/2">
                     <span className="tracking-widest uppercase font-bold md:text-sm text-(--color-primary) font-body">Fullstack Web Developer & Code Enthusiast</span>
                     <h1 className="font-bold text-5xl md:text-6xl text-(--gray-900) mt-5 font-headline">Shaping Dreams Into <span className="bg-linear-to-r from-(--color-primary) to-(--color-secondary) bg-clip-text text-transparent">Digital Excellence</span></h1>
@@ -15,7 +15,7 @@ export default function Hero() {
                     </div>
                 </div>
                 <div className="hero-image">
-                    <img src={personalImage} alt="Leonardo Pires" width={500} className="pointer-events-none"/>
+                    <img src={personalImage} alt="Leonardo Pires" className="pointer-events-none"/>
                 </div>
             </div>
         </section>
