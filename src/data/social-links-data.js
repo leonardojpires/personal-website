9889b9aaf84bd9f2f1dbda3b5998711d835543ea
leonardojpires <leonardojpires2006@gmail.com>
2026-03-06 12:19:38 +0000

    corrected LinkedIn link on social-links-data.js file

--- src/data/social-links-data.js
+++ src/data/social-links-data.js
@@ -2,7 +2,7 @@ import { FiGithub, FiLinkedin, FiTwitter, FiMail } from "react-icons/fi";
 
 const socialLinks = [
     { icon: FiGithub, label: "GitHub", url: "https://github.com/leonardojpires" },
-    { icon: FiLinkedin, label: "LinkedIn", url: "https://github.com/leonardojpires" },
+    { icon: FiLinkedin, label: "LinkedIn", url: "https://www.linkedin.com/in/leonardo-pires-4a9221307/" },
     { icon: FiMail, label: "Email", url: "mailto:leocontacto12@gmail.com" },
 ];
 
