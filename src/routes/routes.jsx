5e70872dfab40d498fc0325b4dd674ced01f2245
leonardojpires <leonardojpires2006@gmail.com>
2026-05-15 18:59:03 +0100

    feat: added ScrollToTop component to react routing

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -3,17 +3,20 @@ import Home from "../pages/Home/Home"
 import Projects from "../pages/Projects/Projects"
 import Contact from "../pages/Contact/Contact"
 import BasePage from "../pages/BasePage/BasePage"
+import ScrollToTop from "@/components/utils/ScrollToTop"
 
 export default function AppRoutes() {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<BasePage />}>
-                    <Route index element={<Home />} />
-                    <Route path="projects" element={<Projects />} />
-                    <Route path="contact" element={<Contact />} />
-                </Route> 
-            </Routes>
+            <ScrollToTop>
+                <Routes>
+                    <Route path="/" element={<BasePage />}>
+                        <Route index element={<Home />} />
+                        <Route path="projects" element={<Projects />} />
+                        <Route path="contact" element={<Contact />} />
+                    </Route>
+                </Routes>
+            </ScrollToTop>
         </BrowserRouter>
     )
 }
